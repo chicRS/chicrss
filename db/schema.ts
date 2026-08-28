5473a5933d34581c257c878ec6b89bb08ee9cb88
chicRS <pubgacpubgac60@gmail.com>
2026-08-28 15:24:15 +0200

    Update schema.ts

--- db/schema.ts
+++ db/schema.ts
@@ -1,10 +1,17 @@
-import { pgTable, serial, text, integer, timestamp } from "drizzle-orm/pg-core";
+import {
+  pgTable,
+  serial,
+  text,
+  integer,
+  timestamp,
+} from "drizzle-orm/pg-core";
 
 export const products = pgTable("products", {
   id: serial().primaryKey(),
   name: text().notNull(),
   price: integer().notNull(),
   category: text().notNull(),
+  description: text().notNull().default(""),
   sizes: text().notNull().default(""),
   badge: text().notNull().default(""),
   image: text().notNull(),
