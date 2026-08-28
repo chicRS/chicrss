4fd29065d70155775cc053a6980d39cdd6b8d833
chicRS <pubgacpubgac60@gmail.com>
2026-08-28 06:10:25 +0200

    Update 20260828060000_add_product_fields.sql

--- netlify/database/migrations/20260825225023_create_products_table/20260828060000_add_product_fields.sql
+++ netlify/database/migrations/20260825225023_create_products_table/20260828060000_add_product_fields.sql
@@ -1,8 +1,8 @@
 ALTER TABLE "products"
-ADD COLUMN "brand" text DEFAULT '' NOT NULL;
+ADD COLUMN IF NOT EXISTS "brand" text DEFAULT '' NOT NULL;
 
 ALTER TABLE "products"
-ADD COLUMN "stock" integer DEFAULT 0 NOT NULL;
+ADD COLUMN IF NOT EXISTS "stock" integer DEFAULT 0 NOT NULL;
 
 ALTER TABLE "products"
-ADD COLUMN "sort_order" integer DEFAULT 0 NOT NULL;
+ADD COLUMN IF NOT EXISTS "sort_order" integer DEFAULT 0 NOT NULL;
